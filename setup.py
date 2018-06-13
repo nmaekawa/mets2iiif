d36f22ea6d29e0d51d334fd098fc7163acd408bf
nmaekawa <nmaekawa@g.harvard.edu>
2018-06-13 10:41:25 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+from setuptools import setup
+from setuptools import find_packages
+
+
+project_name='mets2iiif'
+
+def get_version(*file_paths):
+    """Retrieves the version from [your_package]/__init__.py"""
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+version = get_version(project_name, "__init__.py")
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    'Click>=6.0',
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name=project_name,
+    version=version,
+    description="translate a mets structural map into a iiif manifest",
+    long_description=readme,
+    author="nmaekawa",
+    author_email='nmaekawa@g.harvard.edu',
+    url='https://github.com/nmaekawa/mets2iiif',
+    packages=[
+        'mets2iiif',
+    ],
+    package_dir={'mets2iiif':
+                 'mets2iiif'},
+    entry_points={
+        'console_scripts': [
+            'mets2iiif=mets2iiif.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='mets iiif manifest hul hx ' + project_name,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
